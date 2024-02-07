1f13c0a41af6692be68cfe6398b4ef1370d99d57
Term <106542089+Term-AZ@users.noreply.github.com>
2024-02-07 12:02:16 -0500

    Update and rename T2.233 to T2.233.s

---
+++ Lab1/T2.233.s
@@ -0,0 +1,147 @@
+
+//DISPLAYS HEX
+.global _start
+.data
+.text
+_start:
+	ldr r3, HEX3_HEX0_BASE @lower order four seven segment displays
+	ldr r4, SW_BASE @slide switches
+	ldr r2, DELAY_LENGTH
+	mov r6, #1
+
+_main_loop:
+	bl _read_switches
+	cmp r1, #0	
+		bgt on
+	cmp r1, #0	
+		beq off
+		
+
+_delay_loop:
+	mov r0, #0b0000000
+	str r0, [r3]
+	
+	cmp r7, #0
+		beq _main_loop
+	sub r7,#1
+	b _delay_loop
+	
+_on_delay_loop:
+	cmp r7, #0
+		beq _main_loop
+	sub r7, #1
+	b _on_delay_loop
+	
+on:
+	eor r6, #1
+	bl _find_number
+	bl _display_hex
+	
+	
+	mov r7, r2
+	cmp r6, #1
+		beq _delay_loop
+	cmp r6, #0
+		beq _on_delay_loop
+	b _main_loop
+	
+_display_hex:
+	str r0, [r3]@load byte into ssd
+	bx lr
+	
+_read_switches:
+	ldrb r1, [r4, #0]@read what switches are on
+	bx lr
+
+_find_number:
+    cmp r1, #1      
+        beq one      
+    cmp r1, #2
+        beq two
+    cmp r1, #3
+        beq three
+    cmp r1, #4
+        beq four
+    cmp r1, #5
+        beq five
+    cmp r1, #6
+        beq six
+    cmp r1, #7
+        beq seven
+    cmp r1, #8
+        beq eight
+    cmp r1, #9
+        beq nine
+	 cmp r1, #10
+		beq a
+    cmp r1, #11
+		beq b
+	cmp r1, #12
+		beq c
+	cmp r1, #13
+		beq d
+	cmp r1, #14
+		beq e
+	cmp r1, #15
+		beq f
+    bx lr
+
+////////////////////////DEFINITIONS////////////////////////
+off:
+	mov r0, #0b0000000
+	str r0, [r3]
+	b _main_loop
+zero:
+	mov r0, #0b0111111
+    bx lr
+one:
+	mov r0, #0b0000110
+    bx lr
+two:
+    mov r0, #0b1011011
+    bx lr
+three:
+    mov r0, #0b1001111
+    bx lr
+four:
+    mov r0, #0b1100110
+    bx lr
+five:
+    mov r0, #0b1101101
+    bx lr
+six:
+    mov r0, #0b1111101
+    bx lr
+seven:
+    mov r0, #0b0000111
+    bx lr
+eight:
+    mov r0, #0b1111111
+    bx lr
+nine:
+    mov r0, #0b1100111
+    bx lr
+a:
+	mov r0, #0b1110111
+	bx lr
+b:
+	mov r0, #0b1111100
+	bx lr
+c:
+	mov r0, #0b0111001
+	bx lr
+d:
+	mov r0, #0b1011110
+	bx lr
+e:
+	mov r0, #0b1111001
+	bx lr
+f:
+	mov r0, #0b1110001
+	bx lr
+
+@ labels for constants and addresses
+LED_BASE:		.word	0xFF200000
+HEX3_HEX0_BASE:	.word	0xFF200020
+SW_BASE:		.word	0xFF200040
+DELAY_LENGTH:	.word	50
