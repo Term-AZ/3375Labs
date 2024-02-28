f548552127551007ab7a91ceff8235da49c7af7b
AugustZilakovs <106542089+Term-AZ@users.noreply.github.com>
2024-02-28 11:53:34 -0500

    Update Lab2.s

--- Lab2/Lab2.s
+++ Lab2/Lab2.s
@@ -11,7 +11,7 @@ _start:
 	LDR r0, =MEM
     LDR R1, =timer     // MPCore private timer base address
     LDR R2, =0             // start counter at 0 
-	LDR R3, =20000000 //timeout = 1/(200 MHz) x 200x10^6 = 1 sec
+	LDR R3, =200000000//timeout = 1/(200 MHz) x 200x10^6 = 1 sec
     ldr r4, =0xff200050 //buttons
     ldr r7, =DISPLAY
 	ldr r6, =DISPLAY2
@@ -43,16 +43,78 @@ _readButton:
     b _readButton
 
 display_lap:
-	ldr r3, [r11]
+	
+	mov r12, #0x0000000
+	//minutes
+	ldrb r5, [r11,#2] //load most significant byte
+	mov r10, #240 //set up AND mask (11110000)
+	AND r5, r5, r10 //AND operation
+	
+	bl _find_number //set binary value
+	
+	lsl r12, #8 //shift register values left by 1 byte
+	
+	ldrb r5, [r11,#2] //relaod most significant byte
+	mov r10, #15 //set up AND mask (00001111)
+	AND r5, r5, r10 //AND operation
+	
+	bl _find_number //set binary value
+	
+	
+	
+	//seconds
+	
+	str r12, [r6] //upload to display
+	mov r12, #0x0000000 //reset r12 to begin next four digits. 
+	
+	ldrb r5, [r11,#1]
+	mov r10, #240
+	AND r5, r5, r10
+	
+	bl _find_number
+	
+	lsl r12, #8
+
+	ldrb r5, [r11,#1]
+	mov r10, #15
+	AND r5, r5, r10
+	
+	bl _find_number
+	
+	
+	//least sig figs (milliseconds)
+	
+	lsl r12, #8
+	
+	ldrb r5, [r11,#0]
+	mov r10, #240
+	AND r5, r5, r10
+	
+	bl _find_number
+	
+	lsl r12, #8
+
+	ldrb r5, [r11,#0]
+	mov r10, #15
+	AND r5, r5, r10
+	
+	bl _find_number
+	
+	
+	str r12, [r7]
+	mov r12, #0x0000000
 	
 	b _readButton
+	
 
 clear:
     mov r2, #0 //clear counter register
     b _readButton
     
 lapStore: //store lap
-	str r2, [r11]
+	str r2, [r11] 
+	MOV R3, #0b011  
+	STR R3, [R1, #0x8] 
     b WAIT
 pause:
     mov r3, #0b0000 //stop timer
@@ -136,30 +198,30 @@ checker:
 b1:
 	add r2, r2, #16
 	sub r2, r2,#10
-	b displayNumber
+	b checker
 b2:
 	add r2, r2, #256
 	sub r2, r2,#160
-	b displayNumber
+	b checker
 b3:
 	ldr r5, =#4096
 	add r2, r2, r5
 	ldr r5, =#2560
 	sub r2, r2, r5
-	b displayNumber
+	b checker
 b4:
 	ldr r5, =#65536
 	add r2, r2, r5
 	ldr r5, =#40960
 	sub r2, r2, r5
-	b displayNumber
+	b checker
 	
 b5:
 	ldr r5, =#1048576
 	add r2, r2, r5
 	ldr r5, =#655360
 	sub r2, r2, r5
-	b displayNumber
+	b checker
 
 b6:
 	mov r2, #0
@@ -168,6 +230,7 @@ displayNumber:
 	str r2, [r0] //restore r2 incase of any updates after
   	mov r12, #0x0000000 //reset display values
 	
+	//minutes
 	ldrb r5, [r0,#2] //load most significant byte
 	mov r10, #240 //set up AND mask (11110000)
 	AND r5, r5, r10 //AND operation
@@ -180,7 +243,12 @@ displayNumber:
 	mov r10, #15 //set up AND mask (00001111)
 	AND r5, r5, r10 //AND operation
 	
-	bl _find_number //set binary value. adds the needed binary data to display said digit. then add this to r12 register
+	bl _find_number //set binary value
+	
+	
+	
+	
+	//seconds
 	
 	str r12, [r6] //upload to display
 	mov r12, #0x0000000 //reset r12 to begin next four digits. 
@@ -199,6 +267,9 @@ displayNumber:
 	
 	bl _find_number
 	
+	
+	//least sig figs (milliseconds)
+	
 	lsl r12, #8
 	
 	ldrb r5, [r0,#0]
@@ -215,6 +286,10 @@ displayNumber:
 	
 	bl _find_number
 	
+	
+	
+	
+	
 	str r12, [r7]
 	mov r12, #0x0000000
 
@@ -223,58 +298,90 @@ displayNumber:
 _find_number:
 	cmp r5, #0
 		beq zero
+	cmp r5, #0
+		beq zero
+		
     cmp r5, #1      
-        beq one      
+        beq one   
+	cmp r5, #16     
+        beq one 
+		
     cmp r5, #2
         beq two
+	cmp r5, #32
+        beq two
+		
     cmp r5, #3
         beq three
+	cmp r5, #48
+        beq three
+		
     cmp r5, #4
         beq four
+    cmp r5, #64
+        beq four
+		
     cmp r5, #5
+        beq five	
+    cmp r5, #80
         beq five
+		
     cmp r5, #6
         beq six
+    cmp r5, #96
+        beq six
+		
     cmp r5, #7
         beq seven
+    cmp r5, #112
+        beq seven
+		
     cmp r5, #8
         beq eight
+    cmp r5, #128
+        beq eight
+		
     cmp r5, #9
         beq nine
+	cmp r5, #144
+        beq nine
+		
 	cmp r5, #10
+		beq zero
+	cmp r5, #160
 		beq zero
     bx lr
 
 ////////////////////////DEFINITIONS////////////////////////
 zero:
-	add r12, r12, #0b0111111 //after the bitshit to the left. the first byte should be free now. add it to r12
+	orr r12, #0b0111111
     bx lr
 one:
-	add r12, r12, #0b0000110
+	orr r12, #0b0000110
     bx lr
 two:
-    add r12, r12, #0b1011011
+    orr r12, #0b1011011
     bx lr
 three:
-    add r12, r12, #0b1001111
+   	orr r12, #0b1001111
     bx lr
 four:
-    add r12, r12, #0b1100110
+    orr r12, #0b1100110
     bx lr
 five:
-    add r12, r12, #0b1101101
+    orr r12, #0b1101101
     bx lr
 six:
-    add r12, r12, #0b1111101
+    orr r12, #0b1111101
     bx lr
 seven:
-    add r12, r12, #0b0000111
+    orr r12, #0b0000111
     bx lr
 eight:
-    add r12, r12, #0b1111111
+    orr r12, #0b1111111
     bx lr
 nine:
-    add r12, r12, #0b1100111
+    orr r12, #0b1100111
     bx lr
 
 
